fae322476e5912a3996ead4581711dfaab19aff1
Jaime Leonel Alvizures González <51186267+cleonelAlv1993@users.noreply.github.com>
2023-10-28 10:52:02 -0600

    Creacion de login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Nav from './Componentes/Nav.js';
 import Signup from './Vistas/Signup.js';
+import Login from './Vistas/Login.js';
 
 function App() {
   return (
     <div className="ContenedorTemporal">
       <Nav/>
       <Signup/>
+      <Login/>
     </div>
   );
 }
